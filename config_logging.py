ff28c854064117eae7c086bf7aee19cefe293adf
Rodrigo Sirio Coelho <rodrigoscoelho@gmail.com>
2024-09-02 15:40:32 -0300

    logging

--- config_logging.py
+++ config_logging.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from claudesync.configmanager.file_config_manager import FileConfigManager
 from claudesync.providers.claude_ai import ClaudeAIProvider
 
@@ -7,7 +8,10 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Add a file handler to log to a file
-file_handler = logging.FileHandler('claude_openai_api.log')
+log_dir = '/var/log/claude_openai_api'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir, exist_ok=True)
+file_handler = logging.FileHandler(os.path.join(log_dir, 'claude_openai_api.log'))
 file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
